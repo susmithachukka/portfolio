a075d7f65f56c821d2c2daefbd9520dc692d774c
susmitha <chukkasusmitha45@gmaiil.com>
2026-03-06 16:07:46 +0530

    portfolio

---
+++ Script.js
@@ -0,0 +1,14 @@
+// Smooth scroll effect
+document.querySelectorAll("nav a").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+// Simple console message
+console.log("Portfolio Loaded Successfully!");
